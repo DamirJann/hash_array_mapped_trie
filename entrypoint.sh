6bcfc9914a8d74efe5b39bf244e718785ac70f7d
damirJann <pilacis@list.ru>
2022-02-20 20:47:10 +0300

    add ci/cd

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-cd "$GITHUB_WORKSPACE" || exit 1
-
-ln -sfn ${GTEST_DIR} ./test_list
-mkdir build
-cd build
-cmake -DCMAKE_C_COMPILER=clang-10 -DCMAKE_CXX_COMPILER=clang++-10 ../
-make
-
-for f in $(find "test_list" -type f -executable); do
-  echo "running test suite " $f
-  ./$f
-done
+#cd "$GITHUB_WORKSPACE" || exit 1
+#
+#ln -sfn ${GTEST_DIR} ./test_list
+#mkdir build
+#cd build
+#cmake -DCMAKE_C_COMPILER=clang-10 -DCMAKE_CXX_COMPILER=clang++-10 ../
+#make
+#
+#for f in $(find "test_list" -type f -executable); do
+#  echo "running test suite " $f
+#  ./$f
+#done
