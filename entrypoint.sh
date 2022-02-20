11ae6e77c951ed8f73a7f4c97b54d7565f59a3ae
damirJann <pilacis@list.ru>
2022-02-20 20:23:53 +0300

    add ci/cd

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ cd build
 cmake -DCMAKE_C_COMPILER=clang-10 -DCMAKE_CXX_COMPILER=clang++-10 ../
 make
 
-for f in $(find test_list -type f -executable); do
+for f in $(find "test_list" -type f -executable); do
   echo "running test suite " $f
   ./$f
 done
