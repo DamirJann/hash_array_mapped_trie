71c06be6c679f4f9c547d4ba3cdfe41e60ec0798
damirJann <pilacis@list.ru>
2022-02-20 19:52:27 +0300

    add ci/cd

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 cd "$GITHUB_WORKSPACE" || exit 1
 
-ln -sfn ${GTEST_DIR} ./test/gtest
+pwd
+ln -sfn ${GTEST_DIR} ./test_list
 mkdir build
 cd build
 cmake -DCMAKE_C_COMPILER=clang-10 -DCMAKE_CXX_COMPILER=clang++-10 ../
