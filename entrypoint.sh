b2354acc90017bfbdfa04c2a19486abb6a2c0cb9
damirJann <pilacis@list.ru>
2022-02-23 13:56:32 +0300

    split cmake to test and hamt

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 cd "$GITHUB_WORKSPACE" || exit 1
-cd hamt
+cd test
 
-cmake  -DCMAKE_C_COMPILER=clang-10 -DCMAKE_CXX_COMPILER=clang++-10 --target=run_tests --build .
+cmake  -DCMAKE_C_COMPILER=clang-10 -DCMAKE_CXX_COMPILER=clang++-10 .
 make
 
 ./run_tests
