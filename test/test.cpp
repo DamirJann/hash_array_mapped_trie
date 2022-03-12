da217a174da1148a4aca9d0480dd5fe027d4a706
damirJann <pilacis@list.ru>
2022-03-12 20:36:23 +0300

    removed cout

--- test/test.cpp
+++ test/test.cpp
@@ -575,7 +575,7 @@ TEST(TRIE, HAPPY_FLOW___INSERTING_THE_SAME_KEY_MANY_TIMES_BY_MANY_THREAD) {
     // arrange
     Trie<int, int> trie;
     int thread_count = 10;
-    int iteration_count = 10000;
+    int iteration_count = 100000;
 
     vector<pthread_t> thread(thread_count);
     vector<vector<void *>> attr(thread_count);
