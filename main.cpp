7006fe14e1a8d95a9b9c4814816644959521f2a4
damirJann <pilacis@list.ru>
2022-05-25 00:20:36 +0300

    [perfomance] add benchmark test

--- main.cpp
+++ main.cpp
@@ -1,40 +1,16 @@
+#include <benchmark/benchmark.h>
+#include <armadillo>
 #include "include/hamt.h"
-#include "include/utils.h"
-#include "include/visualize.h"
-#include <assert.h>
-
-#include <random>
-#include <pthread.h>
 
 
-int main(){
-    Hamt<int, int> hamt;
-    int count = 34;
-
-    // act & assert
-    for (int i = 30; i < count; i++) {
-        hamt.insert((i*i*i)%10, i);
+// Define another benchmark
+static void BM_StringCopy(benchmark::State& state) {
+    for (auto _ : state){
+        unsigned int microseconds = 10000000;
+        usleep(microseconds);
     }
-    hamt.insert(40, 8);
-//
-    for (int i = 0; i < count; i++) {
-//        assert(hamt.lookup(i) == hamt.createSuccessfulLookupResult(i));
-    }
-
-//    hamt.remove(0);
-//    hamt.remove(1);
-    for (int i = 0; i < count; i++) {
-//        Hamt<int,int>::RemoveResult r = hamt.remove(i);
-//        assert(r == hamt.createSuccessfulRemoveResult(i));
-    }
-//
-//    for (int i = 0; i < count; i++) {
-//        ASSERT_EQ(hamt.lookup(i), LOOKUP_NOT_FOUND);
-//    }
-
-    ofstream f = std::ofstream("graph.txt");
-    visualize(f, &hamt);
-    system("dot -Tpng ./graph.txt -o graph.png");
-    f.close();
 
 }
+BENCHMARK(BM_StringCopy)->Repetitions(5)->Iterations(1);
+
+BENCHMARK_MAIN();
