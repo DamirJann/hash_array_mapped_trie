a7824d14e39c308f7b892abf6cb0a43c744e70ca
damirJann <pilacis@list.ru>
2022-05-25 00:35:01 +0300

    [perfomance] hide benchmark test

--- test/unit_test.cpp
+++ test/unit_test.cpp
@@ -11,7 +11,7 @@
 #include <gtest/gtest.h>
 #include <random>
 
-int averageIterationCount = 50000;
+int averageIterationCount = 200'000;
 
 TEST(BITMAP, HAPPY_FLOW__IS_SET_TRUE_AT_THE_BEGINNING_OF_BITMAP) {
     // arrange
