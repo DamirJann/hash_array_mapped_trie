8c96a58e0a35febd8cff801313ef9f763fcf280d
damirJann <pilacis@list.ru>
2022-05-30 16:10:57 +0300

    [benchmark] hide benchmark test

--- test/run_test.sh
+++ test/run_test.sh
@@ -24,14 +24,16 @@ test_names=(
 )
 
 for test_name in ${test_names[@]}; do
+  printf "THREAD_COUNT=${THREAD_COUNT}, OPERATION_COUNT=${INSERT_COUNT}\n" >> ${FOLDER_NAME}/${GENERAL_NAME}
+  printf "THREAD_COUNT=${THREAD_COUNT}, OPERATION_COUNT=${INSERT_COUNT}\n" >> ${FOLDER_NAME}/${test_name}.csv
+
   for i in $(seq 1 1 ${TEST_COUNT})
   do
-     touch ${FOLDER_NAME}/${test_name}.txt
      /home/damire/space/hash_array_mapped_trie/test/cmake-build-debug/benchmark_test --benchmark_out_format=csv --benchmark_out=tmp --benchmark_filter=${test_name}
-     sed '12!d' tmp >> ${FOLDER_NAME}/general.txt
-     sed '12!d' tmp >> ${FOLDER_NAME}/${test_name}.txt
+     sed '12!d' tmp >> ${FOLDER_NAME}/${GENERAL_NAME}
+     sed '12!d' tmp >> ${FOLDER_NAME}/${test_name}.csv
   done
-  printf '\n' >> ${FOLDER_NAME}/general.txt
+  printf '\n' >> ${FOLDER_NAME}/${GENERAL_NAME}
 done
 
 
