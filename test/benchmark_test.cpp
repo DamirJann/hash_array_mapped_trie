4fdad8b9db9f32a868fe01688da452d960c26190
damirJann <pilacis@list.ru>
2022-06-01 13:47:06 +0300

    [benchmark] changed benchmark

--- test/benchmark_test.cpp
+++ test/benchmark_test.cpp
@@ -1,37 +1,26 @@
 #include <benchmark/benchmark.h>
 #include "../include/hamt.h"
 #include <unordered_map>
-#include <armadillo>
-#include "cds/init.h"
-#include "cds/gc/hp.h"
-#include "cds/container/michael_kvlist_hp.h"
 
 int threadCount = stoi(std::getenv("THREAD_COUNT"));
 int insertCount = stoi(std::getenv("INSERT_COUNT"));
 
-// -----------------------------------------------------------------
-// -----------------------------------------------------------------
-
 static void Hamt_Insert(benchmark::State &state) {
-    mutex mutex;
     for (auto _: state) {
         Hamt<int, int> hamt;
         vector<pthread_t> thread(threadCount);
         vector<vector<void *>> attr(threadCount);
         for (int i = 0; i < attr.size(); i++) {
-            attr[i] = {&hamt, new int(i), new int(insertCount / threadCount), &mutex};
+            attr[i] = {&hamt, new int(i), new int(insertCount / threadCount)};
         }
         for (int i = 0; i < thread.size(); i++) {
             pthread_create(&thread[i], nullptr, [](void *args) -> void * {
                 auto *hamt = (Hamt<int, int> *) (*static_cast<vector<void *> *>(args))[0];
                 int *id = (int *) (*static_cast<vector<void *> *>(args))[1];
                 int *averageIterationCount = (int *) (*static_cast<vector<void *> *>(args))[2];
-                std::mutex *mutex = (std::mutex *) (*static_cast<vector<void *> *>(args))[3];
 
                 for (int i = *id * (*averageIterationCount); i < (*id + 1) * (*averageIterationCount); i++) {
-                    mutex->lock();
                     hamt->insert(i, i);
-                    mutex->unlock();
                 }
                 pthread_exit(nullptr);
             }, &attr[i]);
@@ -45,78 +34,6 @@ static void Hamt_Insert(benchmark::State &state) {
 }
 
 BENCHMARK(Hamt_Insert)->Repetitions(1)->Iterations(1);
-
-// -----------------------------------------------------------------
-// -----------------------------------------------------------------
-
-static void Hamt_Lookup(benchmark::State &state) {
-    Hamt<int, int> hamt;
-    for (int i = 0; i < threadCount * insertCount; i++) {
-        hamt.insert(i, i);
-    }
-    for (auto _: state) {
-        vector<pthread_t> thread(threadCount);
-        vector<vector<void *>> attr(threadCount);
-        for (int i = 0; i < attr.size(); i++) {
-            attr[i] = {&hamt, new int(i), new int(insertCount / threadCount)};
-        }
-        for (int i = 0; i < thread.size(); i++) {
-            pthread_create(&thread[i], nullptr, [](void *args) -> void * {
-                auto *hamt = (Hamt<int, int> *) (*static_cast<vector<void *> *>(args))[0];
-                int *id = (int *) (*static_cast<vector<void *> *>(args))[1];
-                int *averageIterationCount = (int *) (*static_cast<vector<void *> *>(args))[2];
-
-                for (int i = *id * (*averageIterationCount); i < (*id + 1) * (*averageIterationCount); i++) {
-                    assert(hamt->lookup(i).value == i);
-                }
-                pthread_exit(nullptr);
-            }, &attr[i]);
-        }
-        for (unsigned long i: thread) {
-            pthread_join(i, nullptr);
-        }
-    }
-    cds::Terminate();
-}
-
-BENCHMARK(Hamt_Lookup)->Repetitions(1)->Iterations(1);
-
-
-// -----------------------------------------------------------------
-// -----------------------------------------------------------------
-
-static void Hamt_Remove(benchmark::State &state) {
-    Hamt<int, int> hamt;
-    for (int i = 0; i < threadCount * insertCount; i++) {
-        hamt.insert(i, i);
-    }
-    for (auto _: state) {
-        vector<pthread_t> thread(threadCount);
-        vector<vector<void *>> attr(threadCount);
-        for (int i = 0; i < attr.size(); i++) {
-            attr[i] = {&hamt, new int(i), new int(insertCount / threadCount)};
-        }
-        for (int i = 0; i < thread.size(); i++) {
-            pthread_create(&thread[i], nullptr, [](void *args) -> void * {
-                auto *hamt = (Hamt<int, int> *) (*static_cast<vector<void *> *>(args))[0];
-                int *id = (int *) (*static_cast<vector<void *> *>(args))[1];
-                int *averageIterationCount = (int *) (*static_cast<vector<void *> *>(args))[2];
-
-                for (int i = *id * (*averageIterationCount); i < (*id + 1) * (*averageIterationCount); i++) {
-                    assert(hamt->remove(i) == true);
-                }
-                pthread_exit(nullptr);
-            }, &attr[i]);
-        }
-        for (unsigned long i: thread) {
-            pthread_join(i, nullptr);
-        }
-    }
-    cds::Terminate();
-}
-
-BENCHMARK(Hamt_Remove)->Repetitions(1)->Iterations(1);
-
 
 BENCHMARK_MAIN();
 
--- test/run_test.sh
+++ test/run_test.sh
@@ -3,8 +3,8 @@
 
 GENERAL_NAME=general.csv
 FOLDER_NAME=benchmark_result
-TEST_COUNT=10
-export INSERT_COUNT=1000000
+TEST_COUNT=100
+export INSERT_COUNT=100000
 export THREAD_COUNT=12
 
 rm -rf ${FOLDER_NAME}
@@ -12,12 +12,9 @@ mkdir ${FOLDER_NAME}
 
 test_names=(
     Hamt_Insert
-
-#    Hamt_Lookup
-#
-#    Hamt_Remove
 )
 
+# shellcheck disable=SC2068
 for test_name in ${test_names[@]}; do
   printf "THREAD_COUNT=${THREAD_COUNT}, OPERATION_COUNT=${INSERT_COUNT}\n" >> ${FOLDER_NAME}/${GENERAL_NAME}
   printf "THREAD_COUNT=${THREAD_COUNT}, OPERATION_COUNT=${INSERT_COUNT}\n" >> ${FOLDER_NAME}/${test_name}.csv
@@ -28,7 +25,21 @@ for test_name in ${test_names[@]}; do
      sed '12!d' tmp >> ${FOLDER_NAME}/${GENERAL_NAME}
      sed '12!d' tmp >> ${FOLDER_NAME}/${test_name}.csv
   done
-  printf '\n' >> ${FOLDER_NAME}/general.txt
+  printf '\n' >> ${FOLDER_NAME}/${GENERAL_NAME}
 done
 
 
+for i in $(seq 1 1 15)
+do
+  export THREAD_COUNT=${i}
+  printf "THREAD_COUNT=${THREAD_COUNT}\n" >> ${FOLDER_NAME}/Hamt_Multithreading_Insert.txt
+  for j in $(seq 1 1 ${TEST_COUNT})
+  do
+    export THREAD_COUNT=${i}
+    /home/damire/space/hash_array_mapped_trie/test/cmake-build-debug/benchmark_test --benchmark_out_format=csv --benchmark_out=tmp --benchmark_filter=Hamt_Insert
+    sed '12!d' tmp >> ${FOLDER_NAME}/Hamt_Multithreading_Insert.txt
+  done
+  printf '\n' >> ${FOLDER_NAME}/Hamt_Multithreading_Insert.txt
+done
+
+rm tmp
