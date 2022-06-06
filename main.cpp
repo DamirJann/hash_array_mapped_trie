8c0c931ae01b4ff6d0d9c07c837a93081672b291
damirJann <pilacis@list.ru>
2022-06-06 23:40:24 +0300

    finished 2.2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
+
 #include "include/hamt.h"
+
+
 int main(){
-    for (int i = 0; i < 10000;i++){
-        Hamt<int, int> h;
-        for (int j = 0; j  < 1'000'000; j++){
-            h.insert(j,j );
-        }
-        cout << "finished" << endl;
-    }
+    cout << 0b110;
+
 }
